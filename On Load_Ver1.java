d9cc8fee7e311f420b30f89279d309ba8d883945
Ivonne R <ivonnerubiouh@gmail.com>
2022-01-14 19:15:15 -0600

    Add files via upload

---
+++ On Load_Ver1.java
@@ -0,0 +1,41 @@
+def execute(){
+
+
+    //create a json object with the details of the current file
+    JsonArray attributeDetailsList = new JsonArray();
+    DocumentVO document = documentService.getDocumentDetails();
+
+                   
+    //set the default value for the revision as Auto
+    String revDefaultValue = "Auto";
+    attributeDetailsList.add(getAttributeJSON(IDocumentAttribute.REV, IDocumentAttribute.STANDARD, IDocumentAttribute.READ_ONLY, revDefaultValue)); // Params: Attribute Name, Type Of Atribute, Editable/Read Only, default value
+
+
+    // set the revision
+    document.setResponseStatus(false);
+
+    document.setDynamicObject(attributeDetailsList.toString());
+
+    return document;
+
+    }
+
+     
+
+// This is a generic method to prepare JSON object. No need to do any change in this method.
+
+private JsonObject getAttributeJSON(String attributeName, boolean isCustomAttribute, boolean editOption, String defaultValue) {
+
+    JsonObject attribute =  new JsonObject();
+
+    attribute.addProperty("attributeName", attributeName);
+
+    attribute.addProperty("isEditable", false);
+
+    attribute.addProperty("isCustomAttribute", isCustomAttribute);
+
+    attribute.addProperty("defaultValue", defaultValue);
+
+    return attribute;
+
+}
