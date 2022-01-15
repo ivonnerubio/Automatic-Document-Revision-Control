0b96e6e1e654b004647197138b9baaaeea38d789
Ivonne R <ivonnerubiouh@gmail.com>
2022-01-14 19:59:46 -0600

    Update Post_Load_Ver1.java

--- Post_Load_Ver1.java
+++ Post_Load_Ver1.java
@@ -1,12 +1,14 @@
 def execute(){
-  
-    //Get the current revision of the document and initialize a global revision variable that will be applied to the file
+    // Get the current revision of the document and initialize a global revision variable that will be applied to the file
     DocumentVO document = documentService.getDocumentDetails();
+    
     String revision_new = document.getRevision();
+    
     String doc_publisher = document.getPublisherName();
     
     if(revision_new=="Auto"){
-    revision_new ="00";
+        
+        revision_new ="00";
     }
     
     // Hash map to retrieve the previous revision
@@ -18,53 +20,53 @@ def execute(){
 
     optionMap.put(RevisionOption.INCLUDE_DEACTIVATED, false);
 
-    Map<String, DocumentVO> revisionsMap = documentService.getDocumentRevisionDetail(document, optionMap); // Here revisionsMap will have issue number as key and DocumentVO as value.
+    // Here revisionsMap will have issue number as key and DocumentVO as value.
+    Map<String, DocumentVO> revisionsMap = documentService.getDocumentRevisionDetail(document, optionMap); 
   
+    // If statement to see if this is a new file or an existing file. If true, it retrieves the revision and save into a String variable
     if(!doc_publisher.equals("Workflow Agent System")){
-    // If statement to see if the revision map is NOT EMPTY
         if(!revisionsMap.isEmpty()){
-     
-        // Retrieve the revision and save into a String variable
+            Map.Entry<String, DocumentVO> entry = revisionsMap.entrySet().iterator().next();
 
-        Map.Entry<String, DocumentVO> entry = revisionsMap.entrySet().iterator().next();
-        String docRef_revision = entry.getKey();
-        DocumentVO document_previous = entry.getValue();
-        String revision_previous = document_previous.getRevision();
-		
-        
-        
-        if(isInteger(revision_previous)){
-          // Convert the string into an integer and add one
-          int revision_pre = Integer.parseInt(revision_previous);
-          revision_pre = revision_pre + 1;
+            String docRef_revision = entry.getKey();
 
+            DocumentVO document_previous = entry.getValue();
 
-          // Turn the revision to an integer and set the new revision number
-          if (revision_pre <= 9) {
-              revision_new = "0" + revision_pre.toString();
-          }
-          else{
-              revision_new = revision_pre.toString();
-          } 
-    	}
-        else{
-          revision_new = "00";
+            String revision_previous = document_previous.getRevision();
+            
+            if(isInteger(revision_previous)){
+                // Convert the string into an integer and add one
+                int revision_pre = Integer.parseInt(revision_previous);
+                revision_pre = revision_pre + 1;
+
+                // Turn the revision to an integer and set the new revision number
+                if (revision_pre <= 9) {
+                    revision_new = "0" + revision_pre.toString();
+                }
+                else{
+                    revision_new = revision_pre.toString();
+                  } 
+            }
+            else{
+                revision_new = "00";
+            }
         }
-      }
-  }
+    }
+
     // Apply the new revision to the file
-    documentContext.getDocVO().revision = revision_new;     
+    documentContext.getDocVO().revision = revision_new;   
+
     documentService.updateDocument(documentContext);
 
 }
 
 public static boolean isInteger(String s) {
-        try { 
-            Integer.parseInt(s); 
-        } catch(NumberFormatException e) { 
-            return false; 
-        } catch(NullPointerException e) {
-            return false;
-        }
-        return true;
+    try { 
+        Integer.parseInt(s); 
+    } catch(NumberFormatException e) { 
+        return false; 
+    } catch(NullPointerException e) {
+        return false;
+    }
+    return true;
 }
