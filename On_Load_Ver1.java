b4188feb0e59bc6fc7e2f40efedf8c561678df70
Ivonne R <ivonnerubiouh@gmail.com>
2022-01-14 19:52:11 -0600

    Update On_Load_Ver1.java

--- On_Load_Ver1.java
+++ On_Load_Ver1.java
@@ -1,29 +1,26 @@
 def execute(){
 
-
     //Create a json object with the details of the current file
     JsonArray attributeDetailsList = new JsonArray();
+    
     DocumentVO document = documentService.getDocumentDetails();
-
                    
     //Set the default value for the revision as Auto
     String revDefaultValue = "Auto";
+    
     attributeDetailsList.add(getAttributeJSON(IDocumentAttribute.REV, IDocumentAttribute.STANDARD, IDocumentAttribute.READ_ONLY, revDefaultValue)); // Params: Attribute Name, Type Of Atribute, Editable/Read Only, default value
-
 
     //Set the revision
     document.setResponseStatus(false);
-
+    
     document.setDynamicObject(attributeDetailsList.toString());
-
+    
     return document;
-
-    }
+    
+}
 
      
-
 // This is a generic method to prepare JSON object. No need to do any change in this method.
-
 private JsonObject getAttributeJSON(String attributeName, boolean isCustomAttribute, boolean editOption, String defaultValue) {
 
     JsonObject attribute =  new JsonObject();
